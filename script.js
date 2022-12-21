c8b1fd09a9e6bf0bf8d5a1b4ca18230189fabb24
kirill2804 <kirill495i@yandex.ru>
2022-12-21 12:20:15 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const profile = ['Oluwatobi', 'Sofela', 'codesweetly.com'];
+
+const [firstName, lastName, website] = profile;
+
+console.log(firstName);
+console.log(lastName);
+console.log(website);
